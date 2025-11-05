a9c90b782dcf1d4e192a1e988a1de85171640635
arturoramoss <arti.ramos.s@gmail.com>
2025-11-05 21:37:38 +0000

    Updated setup

--- setup/create_integration.sql
+++ setup/create_integration.sql
@@ -1,7 +1,21 @@
 create or replace api integration github_integration
-    api_provider = 'git_https_api'   -- Using GitHub's HTTPS API
-    api_allowed_prefixes = ('https://github.com/arturoramoss')  -- GitHub API prefix
+    api_provider = git_https_api   -- Using GitHub's HTTPS API
+    api_allowed_prefixes = ('https://github.com')  -- GitHub API prefix
     enabled = true
     allowed_authentication_secrets = all  -- This means all available authentication methods (OAuth, Personal Tokens, etc.)
-    api_user_authentication = (type = snowflake_github_app)  -- Enable OAuth authentication (via GitHub OAuth App)
-    comment = 'Integration to access GitHub repository API';
+    comment = 'Integration to access GitHub repository API'
+    ;
+
+CREATE OR REPLACE SECRET snowflake_learning_db.public.github_pat
+  TYPE = PASSWORD
+  USERNAME = ''
+  PASSWORD = ''
+  ;
+
+
+-- Didn't work so created the repository manually
+-- CREATE OR REPLACE GIT REPOSITORY dbt_snowflake_test
+--   ORIGIN = 'https://github.com/arturoramoss/dbt-snowflake-test.git'
+--   API_INTEGRATION = ( github_integration )
+--   GIT_CREDENTIALS = ( snowflake_learning_db.public.github_pat )
+-- ;
