87b7f13bcf00c0f8c7be6838c6aadadb26cf3a95
arturoramoss <arti.ramos.s@gmail.com>
2025-11-05 22:04:06 +0000

    UPDATED profile

--- setup/create_integration.sql
+++ setup/create_integration.sql
@@ -19,3 +19,21 @@ CREATE OR REPLACE SECRET snowflake_learning_db.public.github_pat
 --   API_INTEGRATION = ( github_integration )
 --   GIT_CREDENTIALS = ( snowflake_learning_db.public.github_pat )
 -- ;
+
+
+-- Create NETWORK RULE for external access integration
+
+CREATE OR REPLACE NETWORK RULE dbt_network_rule
+  MODE = EGRESS
+  TYPE = HOST_PORT
+  -- Minimal URL allowlist that is required for dbt deps
+  VALUE_LIST = (
+    'hub.getdbt.com',
+    'codeload.github.com'
+    );
+
+-- Create EXTERNAL ACCESS INTEGRATION for dbt access to external dbt package locations
+
+CREATE OR REPLACE EXTERNAL ACCESS INTEGRATION dbt_ext_access
+  ALLOWED_NETWORK_RULES = (dbt_network_rule)
+  ENABLED = TRUE;
