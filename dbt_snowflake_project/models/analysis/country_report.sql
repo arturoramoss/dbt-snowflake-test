277d05f44b8d183d448384b714027d6c2f6e9c23
arturoramoss <arti.ramos.s@gmail.com>
2025-11-08 03:00:42 +0000

    Added blue green deployment operations

--- dbt_snowflake_project/models/analysis/country_report.sql
+++ dbt_snowflake_project/models/analysis/country_report.sql
@@ -1,5 +1,5 @@
 select
-    current_timestamp() as run_date
+    convert_timezone('Australia/Sydney',current_timestamp()) as run_date
     , country
     , count(1) as city_count
 from {{ ref('country') }}
